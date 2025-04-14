48e17c08a7e083a8a8d92ffa607ff128bd1daf8c
dhyaa0 <mohkerrouche9@gmail.com>
2025-04-15 00:22:31 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,54 @@
+const Notifications = document.querySelector("ul");
+const Success = document.querySelector("li");
+const btns = document.querySelectorAll("button");
 
-const item = document.querySelectorAll("li")
-const list = document.querySelector("ul")
 
-item.forEach(element =>{
-    element.addEventListener("dragstart",(e)=>{
-        setTimeout(() => e.target.classList.add("active"),0)
-    })
-    element.addEventListener("dragend",(e)=>{
-        e.target.classList.remove("active")
-    })
-})
+const details = {
+    Success: {
+        icon: "fa-solid fa-circle-check",
+        text: "Success: This is a success toast",
+    },
+    Error: {
+        icon: "fa-solid fa-circle-xmark",
+        text: "Error: This is an error toast",
+    },
+    Warning: {
+        icon: "fa-solid fa-circle-exclamation",
+        text: "Warning: This is a warning toast",
+    },
+    Info: {
+        icon: "fa-solid fa-solid fa-circle-info",
+        text: "Info: This is an infomartion toast",
+    },
+}
 
-list.addEventListener("dragover",(e)=>{
-    e.preventDefault()
-    const elementActiveClass = document.querySelector(".active")
-    const elementNormal = [...list.querySelectorAll("li:not(.active)")]
-    let nextSebling = elementNormal.find(Sebling => {
-        return e.clientY <= Sebling.offsetTop + Sebling.offsetHeight / 2;
-    })
-    list.insertBefore(elementActiveClass,nextSebling)
+function Remove1(e) {
+    e.classList.add("hide")
+    setTimeout(() => {
+        e.remove()
+    }, 290)
+}
+
+function toats(e) {
+    return `<div>
+    <i class="fa-sharp ${details[e].icon}"></i>
+    <span>${details[e].text}</span>
+    </div>
+    <i onclick="Remove1(this.parentElement)" class="fa-sharp fa-solid fa-xmark"></i>`
+}
+
+
+btns.forEach(element => {
+    element.addEventListener("click", () => {
+        const li = document.createElement("li")
+        li.setAttribute("id", `${element.id}Toast`)
+        li.innerHTML = toats(element.id)
+        Notifications.appendChild(li)
+        setTimeout(() => {
+            li.classList.add("hide")
+            setTimeout(() => {
+                li.remove()
+            }, 290)
+        }, 5000)
+    });
 })
